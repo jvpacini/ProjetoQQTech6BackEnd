95cb8af4d3ed873d2f3ab0902d585edd58c2d3a4
joao.pacini <joao.pacini@catijr.com.br>
2024-06-20 17:55:35 -0300

    Advances with associative tables

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const perfilRoutes = require("./routes/PerfilRoutes");
 const moduloRoutes = require("./routes/ModuloRoutes");
 const transacaoRoutes = require("./routes/TransacaoRoutes");
 const funcaoRoutes = require("./routes/FuncaoRoutes");
+const moduloFuncoesRoutes = require("./routes/ModuloFuncaoRoutes");
+const perfilModulosRoutes = require("./routes/PerfilModuloRoutes");
+const moduloTransacaoRoutes = require("./routes/ModuloTransacaoRoutes");
 
 const app = express();
 app.use(express.json());
@@ -13,6 +16,9 @@ app.use("/api", perfilRoutes);
 app.use("/api", moduloRoutes);
 app.use("/api", transacaoRoutes);
 app.use("/api", funcaoRoutes);
+app.use("/api", perfilModulosRoutes);
+app.use("/api", moduloFuncoesRoutes);
+app.use("/api", moduloTransacaoRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
